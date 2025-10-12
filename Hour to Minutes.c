cf81ced80550dd86e8adee02e4ced8415970642d
RomitPatel07 <vijaykpatel6680@gmail.com>
2025-10-12 13:51:18 +0530

    Delete Hour to Minutes.c

--- Hour to Minutes.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{printf("Enter Time in Hour : ");
-float a,b;
-scanf("%f",&a);
-b=a*60; // formula for Hour to Minutes
-printf("Time in Minutes : %f hour * 60=%f minutes ",a,b);
-return 0;
- }
