fb38533138ec2df337b698f497f96676995910ed
RomitPatel07 <vijaykpatel6680@gmail.com>
2025-10-12 13:53:50 +0530

    Delete Net salary.c

--- Net salary.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-float g;
-printf("Enter Gross Salary in Rs : ");
-scanf("%f",&g);
-printf("Net salary = %f Rs + (0.1 * %f) Rs - (0.03* %f) Rs = %f Rs", g,g,g,g+(g*0.1)-(g*0.03));
-return 0;
-}
