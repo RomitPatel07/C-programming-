91eb0eef06c6a73a218b9499ded082ec09b50176
RomitPatel07 <vijaykpatel6680@gmail.com>
2025-10-12 13:53:11 +0530

    Delete Minute to Hour - Copy.c

--- Minute to Hour - Copy.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{printf("Enter Time in Minute  : ");
-float a,b;
-scanf("%f",&a);
-b=a/60; // formula for Minute to Hour Conversion
-printf("Time in Hour : %f Minute / 60 = %f Hour ",a,b);
-return 0;
- }
